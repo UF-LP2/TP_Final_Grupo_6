a28d1e29127065f4b51b2a7d047d4ec6c95edc3d
camilaliaudat <camilaliaudat@gmail.com>
2022-11-07 20:06:30 -0300

    hicimos todas las funciones de ruteo, y las clases con sus enums

---
+++ Clases_Enums.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Constantes
+{
+    public const int CostoEnvioCamioneta=50;
+    public const int CostoEnvioFurgoneta=50;
+    public const int CostoEnvioFurgon=50;
+}
+public enum TipoPedido
+{
+    express = 1, normal = 3, diferido = 4
+}
+public enum TipoVehiculo
+{
+    camioneta, furgoneta, furgon
+}
+public enum TipoLineaPedido
+{
+    Electrodomesticos, LineaBlanca, Electronicos, Televisores
+}
+public enum TipoArticulo
+{
+    heladera, lavarropa, cocina, calefon, termotanque, secarropa, microondas, freezer, licuadora, exprimidor,
+    rallador, cafetera, molinillo, computadora, impresora, televisor, celular
+}
+public enum Barrio
+{
+    Liniers = 0, Mataderos = 2, ParqueAvellaneda = 2, VillaLuro = 1, VelezSarfield = 1, Floresta = 1, MonteCastro = 1, Versalles = 3, VillaReal = 3, VillaDevoto = 3, VillaDelParque = 1, VillaSantaRita = 1,
+    VillaGralMitre = 1, LaPaternal = 1, VillaCrespo = 1, Chacarita = 1, Agronomia = 1, ParqueChas = 1, VillaUrtuzar = 1, VillaPueyrredon = 3, VillaUrquiza = 3, Coghlan = 3, Saavedra = 3, Nuñez = 3, Belgrano = 1, Colegiales = 1,
+    Palermo = 1, Recoleta = 1, Almagro = 1, Caballito = 1, ParqueChacabuco = 1, Flores = 1, VillaLugano = 2, VillaRiachuelo = 2, VillaSoldati = 2, NuevaPompeya = 2, Boedo = 1, Barracas = 2, ParquePatricios = 2, LaBoca = 2,
+    Constitucion = 1, SanTelmo = 1, PuertoMadero = 1, SanNicolas = 1, Montserrat = 1, Balvanera = 1, SanCristobal, Retiro = 1, SanIsidro = 3, LaMatanza = 3, TresDeFebrero = 3, VicenteLopez = 3, Ituzaingo = 3,
+    SanMartin = 3, Hurlingham = 3, Moron = 3, LomasDeZamora = 1, Lanus = 2, Avellaneda = 2
+} // le asignamos 1, 2 o 3 dependiendo del recorrido al que pertenezca 
+
+public class Cocimundo
+{
+    public string nombre;
+    public Pedidos deposito;
+    public List<Vehiculos> ListaVehiculos = new List<Vehiculos>(3);
+    public List<Pedidos> ListaPedidos = new List<Pedidos>();
+    // las siguientes listas estan inicialmente vacias
+    public List<Pedidos> ListaDeRecorrido1 = new List<Pedidos>();
+    public List<Pedidos> ListaDeRecorrido2 = new List<Pedidos>();
+    public List<Pedidos> ListaDeRecorrido3 = new List<Pedidos>();
+    
+}
+public class Pedidos
+{
+    public int ID; // identificador del pedido, se genera automaticamente de forma random
+    public TipoPedido Pedido;// express normal o diferido
+    public Clientes cliente; // puntero al cliente que ordeno ese pedido
+    public List<Articulos> ListaDeArticulos; // es una Lista de tipo articulos !!// vamos a tener una funciòn que nos  retorne el volumen total del pedido, porque tiene que entregarse todo en una misma tirada-->volumenTotal()
+    // va a tener una funcion que devuelve el peso total de todos los articulos de ese pedido--> PesoTotal();
+    public bool enviado; // se setea en true si fue enviado, sino se pone en false
+    public int CostoEnvio; //se setea dependiendo de que vehiculo lo lleve
+
+}
+public class Vehiculos
+{
+    public TipoVehiculo Vehiculo;//enum que vehiculo es 
+    public float pesoMaxDeCarga; //camioneta=1200-15kg de la rampa- furgoneta=3500-15kg de la rampa- furgon= 4900 (kg)
+    public float volumenDeCarga;//camioneta=5.4937-0.02 de la rampa- furgoneta=17-0.02 de la rampa- furgon=18 (m3)
+    public float consumoNafta;//camioneta=6.1- furgoneta=6.9- furgon=8.9(l/100km)
+    public float tanqueNafta;// camioneta =50litros- furgoneta= 90litros- furgon=90 litros 
+    public float danio = 0; // se incrementa con los viajes
+    public float kmPorViaje; //se los pasamos de func distancia
+    public int cantViajes; // cantidad de viajes que se hicieron, ya que si es una camioneta puede hacer hasta 4 viajes por día
+}
+
+public class Articulos
+{
+    public TipoLineaPedido CategoriaPedido;
+    public TipoArticulo TipoDeArticulo;//enum con todos los articulos
+    public float Peso; // peso en kg del producto teniendo en cuenta su envoltorio TipoLineaPedido CategoriaPedido; // si pertenece a linea blanca, etc
+    public float largo; //Volumen es un enum donde definimos los volumenes d cada articulo
+    public float ancho;
+    public float alto;
+   
+  
+    // vamos a tener una funcion que calcula el volumen total
+}
+public class Clientes
+{
+    public string nombre;
+    public string apellido;
+    public string DNI;
+    public string Direccion;
+    public float distancia_a_Liniers;
+    private Barrio Barrio;
+}
---
+++ Ruteo.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_final
+{
+    internal class Ruteo
+    {
+
+        public void AsignarRecorrido(Cocimundo cocimundo)
+        {
+            int j = 0;
+            while (j < cocimundo.ListaPedidos.Count())
+            {
+                if (cocimundo.ListaPedidos[j].cliente.Direccion == "1" || cocimundo.ListaPedidos[j].cliente.Direccion == "0")
+                //la direccion pertenece al recorrido 1  o es de Liniers
+                {
+                    cocimundo.ListaDeRecorrido1.Add(cocimundo.ListaPedidos[j]);//guardo ese pedido en mi sublista del recorrido 1
+
+                }
+                else if (cocimundo.ListaPedidos[j].cliente.Direccion == "2")
+                //la direccion pertenece al recorrido 2
+                {
+                    cocimundo.ListaDeRecorrido2.Add(cocimundo.ListaPedidos[j]);//guardo ese pedido en mi sublista del recorrido 2
+                }
+                else
+                //la direccion pertenece al recorrido 3
+                {
+                    cocimundo.ListaDeRecorrido3.Add(cocimundo.ListaPedidos[j]);//guardo ese pedido en mi sublista del recorrido 3
+                }
+                j++;
+            }
+
+        }
+        public List<Pedidos> Ruteo_Por_Recorrido(Cocimundo cocimundo, List<Pedidos> ListaDePedidos, Vehiculos vehiculo, List<Pedidos> ListaSobrantes)
+        {
+            int ContadorNafta = 0;
+            Pedidos auxiliar; // una variable auxiliar del tipo pedido
+            List<Pedidos> ListaAux = new List<Pedidos>();
+            ListaAux[0] = cocimundo.deposito; //Deposito seria la direccion del donde está el deposito el primero en la lista del ruteo será Liniers que es de donde partimos, no sumamos distancia ya que es km =0
+            ListaAux[1] = Distancias(ListaAux[0], ListaDePedidos, vehiculo);//la ciudad que este mas cerca al deposito la visito primero 
+            vehiculo.kmPorViaje = ListaAux[1].cliente.distancia_a_Liniers; // agregamos los km que hay a la ciudad más cercana
+            for (int i = 2; i < ListaAux.Count(); i++)
+            {
+                auxiliar = Distancias(ListaAux[ListaAux.Count()], ListaDePedidos, vehiculo);
+                if (auxiliar == ListaAux[i - 1]) // significa que no puedo entregar mas pedidos porque no me alcanza la nafta
+                {
+                    ContadorNafta++; // lo usamos para tener una condición a la hora de copiarnos la lista y eso, porque si ponemos el for directamente lo hara sin importar si la nafta me alcanzo o no
+                    break; // entonces dejo de recorrer la lista 
+                }
+                else
+                {
+                    ListaAux.Add(auxiliar); // lo guardo en mi lista de pedidos, ya que la nafta me alcanza <3
+                }
+            }
+            if (ContadorNafta == 1)//significa que no me alcanzo la nafta por ende me guardo los pedidos que siguen en la lista en una auxiliar, para hacerlos en otra instancia
+            {
+                for (int k = 0; k < ListaSobrantes.Count(); k++)
+                {
+                    for (int j = k; j < ListaDePedidos.Count(); j++)
+                    {
+                        ListaSobrantes[k] = ListaDePedidos[j];
+                        return ListaAux; // retorno la lista del ruteo, y me guardo en mi listasobrantes los pedidos que no he podido entregar
+                    }
+                }
+            }
+            else
+            {
+                return ListaAux; // es la lista final del ruteo, la cual  esta ordenada de forma tal de ahorrar en el consumo de nafta
+            }
+
+        }
+        public Pedidos Distancias(Pedidos Pedido, List<Pedidos> ListaPedidos, Vehiculos vehiculo)
+        {
+            int ContadorNafta = 0;
+            Pedidos ProxPedido = null; //es un elemento auxiliar del tipo pedido
+            if (ListaPedidos.Count() == 1)// significa que llegue al ultimo pedido de mi recorrido
+            {
+                ProxPedido = ListaPedidos[0]; // es el único elemento por ende es el primero de mi lista
+                double distancia_mas_Cercano = Math.Sqrt(Math.Pow(Pedido.cliente.distancia_a_Liniers, 2) + Math.Pow(ProxPedido.cliente.distancia_a_Liniers, 2)); // vuelvo a calcular la distancia entre mi nodo actual y el elegido como mi proximo pedido
+                vehiculo.kmPorViaje = vehiculo.kmPorViaje + (float)distancia_mas_Cercano;
+                if (!VerificarNafta(vehiculo)) // si la nafta de mi vehículo no me alcanza
+                {
+                    ContadorNafta++;
+                    vehiculo.kmPorViaje = vehiculo.kmPorViaje - (float)distancia_mas_Cercano; // retrocedo al momento previo de añadir ese pedido a mi recorrido
+                    ProxPedido = Pedido; // retorno el anterior a él
+                    return ProxPedido;
+                }
+                else
+                {
+                    ListaPedidos.RemoveAt(ProxPedido.ID);//la borramos porque ya pertenece a la ruta de entrega, y pasara a ser mi nuevo nodo de actual
+                    return ProxPedido;
+                }
+            }
+            for (int i = 0; i < ListaPedidos.Count(); i++) // recorro la lista de los pedidos que me quedan por hacer
+            {
+
+                if (i == 0)
+                {
+                    ProxPedido = ListaPedidos[i];
+                }
+                double distancia = Math.Sqrt(Math.Pow(Pedido.cliente.distancia_a_Liniers, 2) + Math.Pow(ListaPedidos[i].cliente.distancia_a_Liniers, 2)); //calculamos la distancia entre mi nodo  actual y los destinos que me quedan por recorrer
+                double distancia_ProxPedido = Math.Sqrt(Math.Pow(ProxPedido.cliente.distancia_a_Liniers, 2) + Math.Pow(ListaPedidos[i].cliente.distancia_a_Liniers, 2));
+                else if (distancia < distancia_ProxPedido)// el de menor distancia pasará a ocupar el lugar de ProxPedido, al terminar de recorrer la lista tendre mi proximo pedido al que debo ir
+                {
+                    ProxPedido = ListaPedidos[i];
+
+                }//guardo la ciudad a menor distancia de mi ultima ciudad visitada
+            }
+            double distancia_del_mas_cercano = Math.Sqrt(Math.Pow(Pedido.cliente.distancia_a_Liniers, 2) + Math.Pow(ProxPedido.cliente.distancia_a_Liniers, 2)); // vuelvo a calcular la distancia entre mi nodo actual y el elegido como mi proximo pedido
+            vehiculo.kmPorViaje = vehiculo.kmPorViaje + (float)distancia_del_mas_cercano; //sumo los km que tengo que hacer para ir hasta ese próximo pedido
+            if (!VerificarNafta(vehiculo))// si la nafta de mi vehículo no me alcanza
+            {
+                ContadorNafta++;
+                vehiculo.kmPorViaje = vehiculo.kmPorViaje - (float)distancia_del_mas_cercano; // retrocedo al momento previo de añadir ese pedido a mi recorrido
+                ProxPedido = Pedido; // devuelvo el anterior a él
+                return //TODO ver como lo devolvemos
+            }
+            else
+            {
+                // el tamanio de la lista y sus componentes, van siendo modificados en cada iteracion de la función distancia llamada en ruteo!
+                ListaPedidos.RemoveAt(ProxPedido.ID);//la borramos porque ya pertenece a la ruta de entrega, y pasara a ser mi nuevo nodo de actual
+                return ProxPedido; //devuelvo el proximo a ir
+            }
+        }
+        public bool VerificarNafta(Vehiculos vehiculo) // vehiculo es un elemento del tipo vehiculo
+        {
+            float kilometros = (vehiculo.consumoNafta * vehiculo.tanqueNafta) / 100;//los kilometros que puedo recorrer con un tanque de nafta
+            if (vehiculo.kmPorViaje <= kilometros * 2)//lo multiplico x2 para tener en cuenta la vuelta
+            {
+                return true; // si me alcanzan los litros  de nafta para hacer los km de ese recorrido
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //ListaPedidosRecorrido es una lista de elementos del tipo pedido, cocimundo es un elemento del tipo Cocimundo
+        int LineaBlanca(List<Pedidos> ListaPedidos, Cocimundo cocimundo) // retorna la cantidad de pedidos que son de linea blanca en un recorrido
+        {
+            int contador = 0;
+            for (int i = 0; i < ListaPedidos.Count(); i++)
+            {
+                for (int j = 0; j < ListaPedidos[i].ListaDeArticulos.Count(); j++)
+                {
+                    if (ListaPedidos[i].ListaDeArticulos[i].CategoriaPedido == TipoLineaPedido.LineaBlanca)
+                    {
+                        contador++;
+                    }
+                }
+            }
+            return contador;
+        }
+        void AsignarVehiculoARecorrido(int cont1, int cont2, int cont3, Cocimundo cocimundo) // funcion que compara los contadores de linea blanca
+        {
+            if (cont1 > cont3 && cont1 > cont2)
+            {
+                cocimundo.ListaVehiculos[0].Vehiculo = TipoVehiculo.furgon;
+                if (cont2 < cont3)
+                {
+                    cocimundo.ListaVehiculos[1].Vehiculo = TipoVehiculo.camioneta;
+                    cocimundo.ListaVehiculos[2].Vehiculo = TipoVehiculo.furgoneta;
+                }
+                else
+                {
+                    cocimundo.ListaVehiculos[1].Vehiculo = TipoVehiculo.furgoneta;
+                    cocimundo.ListaVehiculos[2].Vehiculo = TipoVehiculo.camioneta;
+                }
+            }
+
+            if (cont2 > cont3 && cont2 > cont1)
+            {
+                cocimundo.ListaVehiculos[1].Vehiculo = TipoVehiculo.furgon;
+                if (cont1 < cont3)
+                {
+                    cocimundo.ListaVehiculos[0].Vehiculo = TipoVehiculo.camioneta;
+                    cocimundo.ListaVehiculos[2].Vehiculo = TipoVehiculo.furgoneta;
+                }
+                else if (cont1 > cont3)
+                {
+                    cocimundo.ListaVehiculos[2].Vehiculo = TipoVehiculo.camioneta;
+                    cocimundo.ListaVehiculos[0].Vehiculo = TipoVehiculo.furgoneta;
+                }
+
+            }
+            if (cont3 > cont2 && cont3 > cont1)
+            {
+                cocimundo.ListaVehiculos[1].Vehiculo = TipoVehiculo.furgon;
+                if (cont1 < cont2)
+                {
+                    cocimundo.ListaVehiculos[0].Vehiculo = TipoVehiculo.camioneta;
+                    cocimundo.ListaVehiculos[1].Vehiculo = TipoVehiculo.furgoneta;
+                }
+                else
+                {
+                    cocimundo.ListaVehiculos[1].Vehiculo = TipoVehiculo.camioneta;
+                    cocimundo.ListaVehiculos[0].Vehiculo = TipoVehiculo.furgoneta;
+                }
+            }
+
+        }
+        void AsignarCostoEnvio(List<Pedidos> ListaPedidos, Vehiculos vehiculo)
+        {
+            if (vehiculo.Vehiculo == TipoVehiculo.camioneta)
+            {
+                for (int i = 0; i < ListaPedidos.Count(); i++)
+                {
+                    ListaPedidos[i].CostoEnvio = Constantes.CostoEnvioCamioneta;
+                }
+
+            }
+            if (vehiculo.Vehiculo == TipoVehiculo.furgoneta)
+            {
+                for (int i = 0; i < ListaPedidos.Count; i++)
+                {
+                    ListaPedidos[i].CostoEnvio = Constantes.CostoEnvioFurgoneta;
+                }
+
+            }
+            else // el envio se hara en el furgón
+            {
+                for (int i = 0; i < ListaPedidos.Count; i++)
+                {
+                    ListaPedidos[i].CostoEnvio = Constantes.CostoEnvioFurgon;
+                }
+            }
+        }
+    }
+
+
+
+
+
+}
